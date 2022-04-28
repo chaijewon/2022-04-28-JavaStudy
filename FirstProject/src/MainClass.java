4c9572d252c672478aa2b64ce0b0c3eece66a5f0
chaijewon <vcandjava@nate.com>
2022-04-28 12:02:08 +0900

    1일차_1

---
+++ FirstProject/src/MainClass.java
@@ -0,0 +1,7 @@
+// ctrl+space => 단축키 ==> 주석 (프로그램에서 제외)
+public class MainClass {
+   public static void main(String[] args) {
+	  System.out.println("Hello Java");// 화면 출력 
+	  // API (Applicaion progamming Interface)
+   }
+}
