dd04ef442ea0cc642ce86c8c149fd62b0cee121d
chaijewon <vcandjava@nate.com>
2022-05-13 16:39:52 +0900

    111

--- 자바9일차-문자열배열응용/src/서울여행_자연.java
+++ 자바9일차-문자열배열응용/src/서울여행_자연.java
@@ -21,7 +21,7 @@ public class 서울여행_자연 {
     					Jsoup.connect("https://korean.visitseoul.net/nature?curPage="+i).get();
     			Elements link=doc.select("ul.article-list li.Item a");
     			// class => 없으면 위에 태그 (class,id) => class => 태그.클래스명
-    			System.out.println("읽은 갯수:"+link.size());
+    			//System.out.println("읽은 갯수:"+link.size());
     			for(int j=0;j<link.size();j++)
     			{
     				String temp="https://korean.visitseoul.net"+link.get(j).attr("href");
@@ -30,10 +30,10 @@ public class 서울여행_자연 {
     				Element title1=doc2.selectFirst("h3.h3");
     				Element msg1=doc2.selectFirst("div.text-area");
     				Element address1=doc2.selectFirst("div.detail-map-wrap div.detail-map-infor dl dd");
-    				System.out.println(title1.text());
-    				System.out.println(msg1.text());
-    				System.out.println(address1.text());
-    				System.out.println("==================================");
+    				//System.out.println(title1.text());
+    				//System.out.println(msg1.text());
+    				//System.out.println(address1.text());
+    				//System.out.println("==================================");
     			    title[k]=title1.text();
     			    msg[k]=msg1.text();
     			    address[k]=address1.text();
@@ -64,19 +64,58 @@ public class 서울여행_자연 {
         	}
         	else if(menu==1)
         	{
-        		
+        		System.out.println("===== 서울 자연&관광 =====");
+        		for(int i=0;i<title.length;i++)
+        		{
+        			if(title[i]!=null) // 비교연산자 (null)
+        			{
+        			  System.out.println((i+1)+"."+title[i]);
+        			}
+        		}
         	}
         	else if(menu==2)
         	{
-        		
+        		System.out.print("상세보기 번호 입력(0~93):");
+        		int detail=scan.nextInt();
+        		System.out.println("장소:"+title[detail]);
+        		System.out.println("소개:"+msg[detail]);
+        		System.out.println("위치:"+address[detail]);
         	}
         	else if(menu==3)
         	{
+        		System.out.print("방문지 입력:");
+        		String loc=scan.next();
         		
+        		for(String s:title)
+        		{
+        		  if(s!=null)
+        		  {  
+        			if(s.contains(loc))
+        			{
+        				System.out.println("★"+s);
+        			}
+        		  }
+        		}
         	}
         	else if(menu==4)
         	{
-        		
+        		try
+        		{
+	        		System.out.print("구/동 입력:");
+	        		String gu=scan.next();
+	        		
+		        		for(int i=0;i<94;i++)
+		        		{
+		        		   
+		        			if(address[i].contains(gu))
+		        		    {
+		        					System.out.println(title[i]);
+		        					System.out.println(address[i]);
+		        					System.out.println("-----------------------------");
+		        		    }
+		        			
+		        		}
+        		}catch(Exception ex) {}
         	}
         	else
         	{
