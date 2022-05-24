73dadd836f0c7ab0f7b30f725cd8efc631f55b2a
chaijewon <vcandjava@nate.com>
2022-05-24 16:10:12 +0900

    1111

--- 자바16일차-멤버변수(사용자정의데이터형)/src/SawonController.java
+++ 자바16일차-멤버변수(사용자정의데이터형)/src/SawonController.java
@@ -1,4 +1,13 @@
 import java.util.*;
+/*
+ *   인스턴스변수는 new를 사용시마다 다로 메모리 생긴다
+ *   static변수 new상관없이 한개의 메모리만 생긴다 (공유)
+ *   242~243page
+ *   246~249page
+ *   -----------------------------------------
+ *   객체지향 용어 
+ *   ----------
+ */
 public class SawonController {
    Sawon[] sawon=new Sawon[20];// 데이터 수집 => 모아서 메모리 
    int index=0;
