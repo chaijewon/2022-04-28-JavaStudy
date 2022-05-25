eb373bef2deda82ad10061a0a98c3d7a8cbc7041
chaijewon <vcandjava@nate.com>
2022-05-25 17:06:41 +0900

    1111

---
+++ 자바17일차-멤버메소드-2/src/인스턴스메소드.java
@@ -0,0 +1,12 @@
+/*
+ *    인스턴스 : 변수 / 메소드 ==> 각각 제어 
+ *    ======
+ *    static 변수 : 데이터를 모아서 관리 (데이터베이스) => 영구적 (오라클) 
+ */
+public class 인스턴스메소드 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
