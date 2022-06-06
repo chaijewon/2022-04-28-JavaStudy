77dd83fb7afaa6e52a7fa4a3b1a4295edca5b4cb
chaijewon <vcandjava@nate.com>
2022-06-07 08:55:23 +0900

    1111

--- 자바23일차-예외처리응용/src/com/sist/client/Client.java
+++ 자바23일차-예외처리응용/src/com/sist/client/Client.java
@@ -1,14 +1,80 @@
 package com.sist.client;
 import java.util.*;
 import java.net.*;
-public class Client {
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+public class Client extends JFrame implements Runnable,ActionListener{
+    JTextArea ta;
+    JTextField tf;
+    JButton startBtn;
+    String name;
+    Socket s;
+    OutputStream out;
+    BufferedReader in;
+    public Client()
+    {
+    	ta=new JTextArea();
+    	JScrollPane js=new JScrollPane(ta);
+    	tf=new JTextField(30);
+    	startBtn=new JButton("연결");
+    	
+    	JPanel p=new JPanel();
+    	p.add(tf);
+    	p.add(startBtn);
+    	
+    	add("Center",js);
+    	add("South",p);
+    	setSize(420, 500);
+    	setVisible(true);
+    	startBtn.addActionListener(this);
+    	tf.addActionListener(this);
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         try
-         {
-        	 Socket s=new Socket("localhost",3355);
-         }catch(Exception ex) {}
+         new Client();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==startBtn)
+		{
+			name=JOptionPane.showInputDialog("이름 입력");
+			try
+			{
+				s=new Socket("localhost",3355);
+				out=s.getOutputStream();
+				in=new BufferedReader(
+						new InputStreamReader(s.getInputStream()));
+			    new Thread(this).start();
+			}catch(Exception ex) {}
+		}
+		else if(e.getSource()==tf)
+		{
+			try
+			{
+				String msg=tf.getText();
+				if(msg.length()<1)
+					return;
+				out.write(("["+name+"]"+msg+"\n").getBytes());
+				tf.setText("");
+			}catch(Exception ex) {}
+		}
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true)
+		{
+			try
+			{
+				String msg=in.readLine();
+				ta.append(msg+"\n");
+			}catch(Exception ex){}
+		}
 	}
 
 }
